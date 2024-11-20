163d4b0a59ea7ef95d1d86bb25b18de5056e52ee
Tglman <tglman@tglman.com>
2024-11-20 22:34:13 +0000

    fix: minor fix in script executor retry logic

--- core/src/main/java/com/orientechnologies/orient/core/command/OSqlScriptExecutor.java
+++ core/src/main/java/com/orientechnologies/orient/core/command/OSqlScriptExecutor.java
@@ -118,12 +118,19 @@ public class OSqlScriptExecutor extends OAbstractScriptExecutor {
               OInternalExecutionPlan sub = statement.createExecutionPlan(scriptContext);
               plan.chain(sub, false);
             }
+            lastRetryBlock = new ArrayList<>();
           }
         }
       }
 
       if (stm instanceof OLetStatement) {
         scriptContext.declareScriptVariable(((OLetStatement) stm).getName().getStringValue());
+      }
+    }
+    if (!lastRetryBlock.isEmpty()) {
+      for (OStatement statement : lastRetryBlock) {
+        OInternalExecutionPlan sub = statement.createExecutionPlan(scriptContext);
+        plan.chain(sub, false);
       }
     }
     return new OLocalResultSet(plan);
